e0e3f53e6716e2bacb2c3f937e146c9bb0dccb65
julienvanderheyden <julien.vanderheyden@student.uliege.be>
2025-05-19 07:27:43 +0000

    stiffness modifs

--- hand control files/object_centric_medium_wrap_2.jl
+++ hand control files/object_centric_medium_wrap_2.jl
@@ -159,7 +159,7 @@ function object_centric_medium_wrap(cylinder_radius)
 
     base_stiffness = 0.05
     phalanx_scaling_factor = 0.5 #phalanx scaling : proximal stiffness bigger than distal
-    finger_scaling_factor = 0.3 #finger scaling : thumb stiffness bigger than little finger
+    finger_scaling_factor = -0.5
     
     stiffnesses = generate_stiffnesses_linear_scaling(base_stiffness, phalanx_scaling_factor, finger_scaling_factor)
     
