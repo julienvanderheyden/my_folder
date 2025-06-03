a3189f22c8f6c8c43034564c87cbb4f5650d333d
julienvdh <julien.vanderheyden@student.uliege.be>
2025-06-03 14:12:30 +0200

    changed object centric lateral pinch initial position

--- hand control files/object_centric_lateral_pinch_2.jl
+++ hand control files/object_centric_lateral_pinch_2.jl
@@ -264,6 +264,8 @@ function object_centric_lateral_pinch(box_width, box_thickness)
     #thumb max extension
     qᵛ[23] = -0.7
     qᵛ[24] = -0.26
+    qᵛ[5] = 1.57
+    qᵛ[4] = 1.0
 
     joint_names = ["rh_WRJ1", "rh_WRJ2", "rh_FFJ1", "rh_FFJ2", "rh_FFJ3", "rh_FFJ4", "rh_MFJ1",
                     "rh_MFJ2", "rh_MFJ3", "rh_MFJ4", "rh_RFJ1", "rh_RFJ2", "rh_RFJ3", "rh_RFJ4", 
