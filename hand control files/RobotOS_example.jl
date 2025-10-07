e2d0e1046823aeedfddfdda5b77b25d41b03a593
julienvdh <julien.vanderheyden@student.uliege.be>
2025-10-07 17:07:30 +0200

    simplified robotos example

--- hand control files/RobotOS_example.jl
+++ hand control files/RobotOS_example.jl
@@ -1,31 +1,21 @@
-#!/usr/bin/env julia
-
 using RobotOS
-@rosimport std_msgs.msg: Int32
-rostypegen()
-using .std_msgs.msg
 
-function callback(msg::Int32, pub_obj::Publisher{Int32})
-    pt_msg = Int32(msg.data, 0.0)
-    publish(pub_obj, pt_msg)
-end
+@rosimport std_msgs.msg: Float64  # import the message definition
+rostypegen()  # generate Julia bindings
 
-function loop(pub_obj)
-    loop_rate = Rate(5.0)
-    while ! is_shutdown()
-        npt = Int32(rand(), 0.0)
-        publish(pub_obj, npt)
-        rossleep(loop_rate)
-    end
-end
+# Initialize ROS node
+init_node("julia_number_publisher")
 
-function main()
-    init_node("rosjl_example")
-    pub = Publisher{Int32}("pts", queue_size=10)
-    sub = Subscriber{Int32}("pose", callback, (pub,), queue_size=10)
-    loop(pub)
-end
+# Create a publisher
+pub = Publisher("/simple_number", std_msgs.msg.Float64)
 
-if ! isinteractive()
-    main()
+rate = Rate(1.0)  # 1 Hz
+
+x = 0.0
+while !is_shutdown()
+    msg = std_msgs.msg.Float64(data = x)
+    publish(pub, msg)
+    println("Published number: $x")
+    x += 0.1
+    sleep(rate)
 end
