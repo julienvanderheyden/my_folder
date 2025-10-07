50a51d15c3a789a1406afa45d37f9cbfefc37bcb
julienvdh <julien.vanderheyden@student.uliege.be>
2025-10-07 16:59:39 +0200

    modified robotOS example

--- hand control files/RobotOS_example.jl
+++ hand control files/RobotOS_example.jl
@@ -1,19 +1,19 @@
 #!/usr/bin/env julia
 
 using RobotOS
-@rosimport geometry_msgs.msg: Point, Pose2D
+@rosimport std_msgs.msg: Int32
 rostypegen()
-using .geometry_msgs.msg
+using .std_msgs.msg
 
-function callback(msg::Pose2D, pub_obj::Publisher{Point})
-    pt_msg = Point(msg.x, msg.y, 0.0)
+function callback(msg::Int32, pub_obj::Publisher{Int32})
+    pt_msg = Int32(msg.data, 0.0)
     publish(pub_obj, pt_msg)
 end
 
 function loop(pub_obj)
     loop_rate = Rate(5.0)
     while ! is_shutdown()
-        npt = Point(rand(), rand(), 0.0)
+        npt = Int32(rand(), 0.0)
         publish(pub_obj, npt)
         rossleep(loop_rate)
     end
@@ -21,8 +21,8 @@ end
 
 function main()
     init_node("rosjl_example")
-    pub = Publisher{Point}("pts", queue_size=10)
-    sub = Subscriber{Pose2D}("pose", callback, (pub,), queue_size=10)
+    pub = Publisher{Int32}("pts", queue_size=10)
+    sub = Subscriber{Int32}("pose", callback, (pub,), queue_size=10)
     loop(pub)
 end
 
