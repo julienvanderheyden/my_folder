6a118bfbd1ce5da0bcc5aad2a7841da50ae7ba7a
julienvdh <julien.vanderheyden@student.uliege.be>
2025-10-09 09:27:13 +0200

    debugging

--- hand control files/ros_julia_sync.jl
+++ hand control files/ros_julia_sync.jl
@@ -37,7 +37,8 @@ function main()
     end
     
     sub = Subscriber{Int32Msg}("/ros_julia_synchronization", callback; queue_size=10)
-    msg = Int32Msg(current_step +1)
+    msg = Int32Msg(current_step + 1)
+    println("Starting the test, sending arm to position", current_step +1)
     publish(pub, msg)
 
 
