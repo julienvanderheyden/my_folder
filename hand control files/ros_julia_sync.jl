75ab5f3f3a4c90d694d769d12a59ba99069d5422
julienvdh <julien.vanderheyden@student.uliege.be>
2025-11-21 15:42:53 +0100

    test on flat rectangles

--- hand control files/ros_julia_sync.jl
+++ hand control files/ros_julia_sync.jl
@@ -16,12 +16,12 @@ function main()
 
     current_step = 1 # step 1 : going to home position
 
-    grasp_type = 1 # 1 for medium wrap, 2 for power sphere, 3 for lateral pinch
-    grasp_parameters = [0.003, 0.01, 0.012, 0.014, 0.017, 0.018, 0.02, 0.03] #, 0.023, 0.025, 0.028, 0.032, 0.038] 
+    #grasp_type = 1 # 1 for medium wrap, 2 for power sphere, 3 for lateral pinch
+    #grasp_parameters = [0.003, 0.01, 0.012, 0.014, 0.017, 0.018, 0.02, 0.03] #, 0.023, 0.025, 0.028, 0.032, 0.038] 
 
-    #grasp_type = 3
-    #grasp_parameters = [[0.025, 0.005], [0.025, 0.005], [0.025, 0.005], [0.025, 0.005], 
-    #[0.025, 0.005], [0.025, 0.005], [0.025, 0.005], [0.025, 0.005], [0.025, 0.005]]
+    grasp_type = 3
+    grasp_parameters = [[0.025, 0.001], [0.0375, 0.001], [0.05, 0.001], [0.025, 0.006], 
+    [0.0375, 0.006], [0.05, 0.006], [0.025, 0.013], [0.0375, 0.013], [0.05, 0.013]]
     function callback(msg)
         if msg.data == 0 && current_step <= 10 
             
