ff57a25e5f9f1afdae89aa7dc1651a127189656a
julienvanderheyden <bilou.vanderheyden@gmail.com>
2025-12-16 16:41:01 +0100

    removed deadzone springs to isolate the soiurce of the problem

--- hand control files/feedback_position_control.jl
+++ hand control files/feedback_position_control.jl
@@ -68,8 +68,8 @@ for joint_id in keys(joints(vm_robot))
     # println("\n")
     add_coordinate!(vm_robot, JointSubspace(joint_id);  id="$(joint_id)_coord")
     @assert ~isnothing(limits.lower) && ~isnothing(limits.upper)
-    add_deadzone_springs!(vm_robot, 0.01, (limits.lower+0.0, limits.upper-0.0), "$(joint_id)_coord")
-    add_component!(vm_robot, LinearDamper(0.0001, "$(joint_id)_coord"); id="$(joint_id)_damper")
+    #add_deadzone_springs!(vm_robot, 0.01, (limits.lower+0.0, limits.upper-0.0), "$(joint_id)_coord")
+    #add_component!(vm_robot, LinearDamper(0.0001, "$(joint_id)_coord"); id="$(joint_id)_damper")
 end
 
 add_coordinate!(vm_robot, CoordSum("rh_FFJ1_coord", "rh_FFJ2_coord"); id="rh_FFJ0_coord")
@@ -107,8 +107,8 @@ println("Virtual Mechanism Built !")
 
 print("Linking real robot and virtual robot ...")
 
-# feedback_stiffness = 0.00001
-feedback_damping = 0.0
+feedback_stiffness = 0.00001
+# feedback_damping = 0.0
 # feedback_damping = 0.00001
 feedback_stiffness = 0.0
 
