2e839c78b1760e0e64285e69c970c940e98982ee
julienvanderheyden <bilou.vanderheyden@gmail.com>
2026-02-11 10:01:13 +0100

    increased grasp timeout

--- VMRobotControl.jl/ros/ROS.jl
+++ VMRobotControl.jl/ros/ROS.jl
@@ -471,8 +471,8 @@ function ros_vm_position_controller(
                     return true
                 end
 
-                if t > 12.0
-                    @info "12 seconds reached, stopping controller"
+                if t > 20.0
+                    @info "20 seconds reached, stopping controller"
                     return true
                 end
             end 
